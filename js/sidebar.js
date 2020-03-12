e40ab8588d829b66092028166b61177db471d140
Jeremy Lee <Taka86@hotmail.co.uk>
2020-03-12 14:50:37 +0000

    final commit

---
+++ js/sidebar.js
@@ -0,0 +1,33 @@
+const mbtn = document.querySelector('.menu-btn');
+const body = document.querySelector('#b-container');
+const sb = document.querySelector('.sidebar');
+const disable = document.querySelector("#disable");
+const burger = document.querySelector(".burger-box");
+
+
+mbtn.addEventListener("click", () => {  
+    body.classList.toggle("sb-class");
+    sb.classList.toggle("sidebar-vis");
+    disable.classList.toggle("disable");
+    burger.classList.toggle("open");
+    const top = document.querySelector(".top");
+    const bottom = document.querySelector(".bottom");
+    top.style.top = "-14px";
+    top.style.left = "-16px";
+    bottom.style.top = "-14px";
+    bottom.style.left = "-16px";
+});
+
+disable.addEventListener("click", () => {
+    body.classList.toggle("sb-class");
+    sb.classList.toggle("sidebar-vis");
+    disable.classList.toggle("disable");
+    burger.classList.toggle("open");
+    const top = document.querySelector(".top");
+    const bottom = document.querySelector(".bottom");
+    top.style.top = "-20px";
+    top.style.left = "";
+    bottom.style.top = "0px";
+    bottom.style.left = "";
+});
+
